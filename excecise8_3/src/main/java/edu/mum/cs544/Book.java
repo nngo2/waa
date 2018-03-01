bde9eb42b6f71f7ffd5b4418073d1b1b1a6273e4
namngo <hnnngo@mum.edu>
2018-03-01 17:26:53 -0600

    Create

---
+++ excecise8_3/src/main/java/edu/mum/cs544/Book.java
@@ -0,0 +1,94 @@
+package edu.mum.cs544;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+public class Book {
+	private int id;
+	
+	@NotEmpty
+	private String title;
+	
+	@Pattern(regexp="\\d{3}-\\d{10}")
+	private String ISBN;
+	
+	@NotEmpty
+	private String author;
+	
+	@NotNull
+	@Min(value=1)
+	private double price;
+	
+	@PastDateConstraint
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate publishedDate;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String title, String iSBN, String author, double price, LocalDate publishedDate) {
+		super();
+		this.title = title;
+		this.ISBN = iSBN;
+		this.author = author;
+		this.price = price;
+		this.publishedDate = publishedDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getISBN() {
+		return this.ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		this.ISBN = iSBN;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public LocalDate getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(LocalDate publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+}
---
+++ excecise8_3/src/main/java/edu/mum/cs544/BookDao.java
@@ -0,0 +1,56 @@
+package edu.mum.cs544;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BookDao implements IBookDao {
+	private static int idCount = 1;
+	private Map<Integer, Book> books = new HashMap<>();
+
+	public BookDao() {
+		add(new Book("Spring MVC 3", "111-1111111110", "Jame Smith", 23.99, LocalDate.of(2018, Month.APRIL, 12)));
+		add(new Book("Spring Boot for Dummy", "111-1111111111", "Jane Doe", 88.99,  LocalDate.of(2018, Month.AUGUST, 15)));
+	}
+
+	@Override
+	public List<Book> getAll() {
+		return new ArrayList<Book>(books.values());
+	}
+
+	@Override
+	public void add(Book book) {
+		synchronized (books) {
+			book.setId(idCount);
+			books.put(idCount, book);
+			idCount++;			
+		}
+	}
+
+	@Override
+	public Book get(int id) {
+		Book result = books.get(id);
+
+		if (result == null) {
+			throw new NoSuchResourceException("Book", id);
+		}
+
+		return result;
+	}
+
+	@Override
+	public void update(int bookId, Book book) {
+		books.put(bookId, book);
+	}
+
+	@Override
+	public void delete(int bookId) {
+		Book removed = books.remove(bookId);
+		if (removed == null) {
+			throw new NoSuchResourceException("Book", bookId);
+		}
+	}
+}
