f674c8e4d19614d5e3da16079ae08ce8fdfde26d
namngo <hnnngo@mum.edu>
2018-03-10 07:58:54 -0600

    Add product list

--- coffeeShop/src/main/resources/data.sql
+++ coffeeShop/src/main/resources/data.sql
@@ -4,3 +4,7 @@ insert into role(role) values ('USER');
 insert into person(email, last_name, first_name, enable) values('admin@coffeeshop.com', 'Admin', 'Admin', 1);
 insert into user(email, password, active) values('admin@coffeeshop.com', '$2a$10$R1kmVEHTHJ90GcejrpmTdOirALp9ZJ7ugMkPSDRKlOhTc1j92WClW', 1);
 insert into user_role (user_id, role_id) values (1,1);
+
+insert into product(description, price, product_name, product_type) values('Hot Dark Black Coffee', 2.5, 'Hot Coffee', 'BREAKFAST');
+insert into product(description, price, product_name, product_type) values('Hot Grren Tea', 1.5, 'Hot Tea', 'BREAKFAST');
+insert into product(description, price, product_name, product_type) values('Hot Beef Noddle Soup', 7.5, 'Pho', 'LUNCH');
