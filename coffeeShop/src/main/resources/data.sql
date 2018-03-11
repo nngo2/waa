68cf16063636a984a51606821233d6b328c6c997
namngo <hnnngo@mum.edu>
2018-03-11 13:32:23 -0500

    Update default data

--- coffeeShop/src/main/resources/data.sql
+++ coffeeShop/src/main/resources/data.sql
@@ -1,6 +1,8 @@
 insert into role(role) values ('ADMIN');
 insert into role(role) values ('USER');
 
+/* all users have the same password = password */
+
 insert into person(email, last_name, first_name, enable) values('admin@coffeeshop.com', 'Admin', 'Admin', 1);
 insert into user(email, password, active) values('admin@coffeeshop.com', '$2a$10$R1kmVEHTHJ90GcejrpmTdOirALp9ZJ7ugMkPSDRKlOhTc1j92WClW', 1);
 insert into user_role (user_id, role_id) values (1,1);
@@ -15,3 +17,4 @@ insert into product(description, price, product_name, product_type, product_imag
 insert into product(description, price, product_name, product_type, product_image) values('Grilled Chicken Special', 2.5, 'Menu 4', 'DINNER', '/images/product4.jpg');
 insert into product(description, price, product_name, product_type, product_image) values('Chicken Tacos Special', 1.5, 'Menu 5', 'DINNER', '/images/product5.jpg');
 insert into product(description, price, product_name, product_type, product_image) values('Gilled Sweetted Salmon Special', 7.5, 'Menu 6', 'DINNER', '/images/product6.jpg');
+insert into product(description, price, product_name, product_type, product_image) values('Vietnames Back Peppeh FisSpecial', 7.5, 'Menu 7', 'DINNER', '/resources/images/fish.jpg');
